eca4070d5c3d049bed8cbc60ed8575708c4c7006
Lazy <2211717435@qq.com>
2024-01-05 21:52:40 +0800

    修改 Header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,26 +6,39 @@
       </div>
       <v-btn
         v-for="link in links"
-        :key="link"
-        :text="link"
+        :key="link.name"
+        :text="link.name"
         variant="text"
-        :to="link"
+        :to="link.to"
         class="mr-4"
+        color="brown-darken-3"
       ></v-btn>
 
       <v-spacer></v-spacer>
-      <v-avatar
-        class="me-4"
-        color="grey-darken-1"
-        image="https://cdn.imlazy.ink:233/img/tup/20240104/aBlackList.png"
-        size="32"
-      ></v-avatar>
+      <div class="d-flex align-center" style="cursor: pointer">
+        <div class="mr-4">未登录</div>
+        <v-avatar
+          class="me-4"
+          color="grey-darken-1"
+          image="https://cdn.imlazy.ink:233/img/tup/20240104/aBlackList.png"
+          size="32"
+        ></v-avatar>
+      </div>
     </v-container>
   </v-app-bar>
 </template>
 
 <script setup lang="ts">
-const links = ["/", "test"];
+const links = [
+  {
+    name: "首页",
+    to: "/",
+  },
+  {
+    name: "关于",
+    to: "about",
+  },
+];
 </script>
 
 <style lang="scss" scoped>
--- typed-router.d.ts
+++ typed-router.d.ts
@@ -40,7 +40,7 @@ import type {
 declare module 'vue-router/auto/routes' {
   export interface RouteNamedMap {
     '/': RouteRecordInfo<'/', '/', Record<never, never>, Record<never, never>>,
-    '/test': RouteRecordInfo<'/test', '/test', Record<never, never>, Record<never, never>>,
+    '/about': RouteRecordInfo<'/about', '/about', Record<never, never>, Record<never, never>>,
   }
 }
 
