13a04059230f8eb6a16ef33c68b33782addad1ce
Lazy <2211717435@qq.com>
2024-01-05 21:19:30 +0800

    优化 LOGO？

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,16 @@
 <template>
   <v-app-bar flat>
     <v-container class="mx-auto d-flex align-center justify-center">
+      <div class="mr-4 logo">
+        <a href="javascript:;" class="text-decoration-none">A Blacklist</a>
+      </div>
       <v-btn
         v-for="link in links"
         :key="link"
         :text="link"
         variant="text"
         :to="link"
-        class="mr-2"
+        class="mr-4"
       ></v-btn>
 
       <v-spacer></v-spacer>
@@ -24,3 +27,13 @@
 <script setup lang="ts">
 const links = ["/", "test"];
 </script>
+
+<style lang="scss" scoped>
+.logo {
+  a {
+    color: unset;
+    text-shadow: 6px -4px 15px #e20c7d, -7px 7px 15px #fd3535;
+    font-weight: 500;
+  }
+}
+</style>
