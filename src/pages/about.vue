0c4dfa1d7c9f6095d7049753309e3eb6343dcde1
Lazy <2211717435@qq.com>
2024-01-06 00:36:30 +0800

    新增 关于页面
    懒得写

---
+++ src/pages/about.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-container>
+    <v-responsive class="align-center text-center fill-height">
+      开源地址：
+      <ul>
+        <li>
+          后端：<a
+            href="https://github.com/LazyCreeper/a-blacklist"
+            target="_blank"
+            >https://github.com/LazyCreeper/a-blacklist</a
+          >
+        </li>
+        <li>
+          前端：<a
+            href="https://github.com/LazyCreeper/a-blacklist-ui"
+            target="_blank"
+            >https://github.com/LazyCreeper/a-blacklist-ui</a
+          >
+        </li>
+      </ul>
+    </v-responsive>
+  </v-container>
+</template>
+
+<script lang="ts" setup>
+//
+</script>
