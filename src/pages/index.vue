d0bd0317018976076834683c5b75260408416bfe
Lazy <2211717435@qq.com>
2024-01-06 16:41:27 +0800

    新增 编辑

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -20,34 +20,109 @@
         :items="serverItems"
         :loading="loading"
         :search="search"
+        hover
         @update:options="loadItems"
       >
         <template v-slot:item.addTime="{ item }">
           {{ new Date(item.addTime).toLocaleString() }}
         </template>
+        <template v-slot:item.operate="{ item }">
+          <v-btn
+            class="mr-4"
+            icon="mdi-pencil-outline"
+            variant="text"
+            size="small"
+            @click="edit(item)"
+          ></v-btn>
+          <v-btn
+            icon="mdi-trash-can-outline"
+            variant="text"
+            size="small"
+            color="red"
+            @click="edit(item)"
+          ></v-btn>
+        </template>
       </v-data-table-server>
     </v-responsive>
   </v-container>
+
+  <v-dialog v-model="dialog.show" persistent width="500">
+    <v-card>
+      <v-card-title> {{ dialog.title }} </v-card-title>
+      <v-card-text>
+        <v-form ref="form">
+          <v-row>
+            <v-col>
+              <v-text-field
+                v-model:model-value="formData.qq"
+                label="QQ"
+                clearable
+                density="compact"
+                variant="outlined"
+                hide-details
+              ></v-text-field>
+            </v-col>
+            <v-col>
+              <v-text-field
+                v-model:model-value="formData.bilibili"
+                label="B站 UID"
+                clearable
+                density="compact"
+                variant="outlined"
+                hide-details
+              ></v-text-field>
+            </v-col>
+          </v-row>
+          <v-row>
+            <v-col>
+              <v-textarea
+                v-model:model-value="formData.reason"
+                label="原因"
+                clearable
+                density="compact"
+                variant="outlined"
+                :rules="[(v) => !!v || '请输入原因']"
+                required
+              ></v-textarea>
+            </v-col>
+          </v-row>
+        </v-form>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn color="red" @click="reset">重置</v-btn>
+        <v-spacer></v-spacer>
+        <v-btn @click="dialog.cancel">取消</v-btn>
+        <v-btn color="primary" :loading="saveBtnLoading" @click="save"
+          >保存</v-btn
+        >
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script lang="ts" setup>
 import { ref } from "vue";
 import axios from "axios";
 import _ from "lodash";
-import type { BlacklistRes, Blacklist } from "@/types";
+import type { BlacklistRes, Blacklist, NyaResponse } from "@/types";
 import { indexStore } from "@/stores";
 const { showMsg } = indexStore();
-const itemsPerPage = ref(5);
+const itemsPerPage = ref(10);
 const headers = ref([
   { key: "id", title: "ID" },
   { key: "qq", title: "QQ" },
   { key: "reason", title: "原因" },
   { key: "addTime", title: "添加时间" },
+  { key: "operate", title: "操作", sortable: false },
 ]);
 const search = ref("");
 const serverItems = ref<Blacklist[]>([]);
 const loading = ref(false);
 const totalItems = ref(10);
+const currentPage = ref(1);
+const _sortBy = ref<Array<{ key: string; order: string }>>([
+  { key: "id", order: "asc" },
+]);
 
 const loadItems = _.throttle(
   async ({
@@ -61,6 +136,8 @@ const loadItems = _.throttle(
   }) => {
     try {
       loading.value = true;
+      currentPage.value = page;
+      _sortBy.value = sortBy;
       const { data }: { data: BlacklistRes } = await axios.get("/v1", {
         params: {
           page,
@@ -81,4 +158,70 @@ const loadItems = _.throttle(
   },
   1000
 );
+
+const form = ref();
+const formData = ref<Blacklist>({
+  id: 0,
+  qq: "",
+  bilibili: "",
+  reason: "",
+  addTime: "",
+});
+
+const reset = () => {
+  form.value.reset();
+};
+
+const dialog = ref({
+  show: false,
+  title: "添加",
+  text: "",
+  cancel: () => {
+    dialog.value.show = false;
+    reset();
+  },
+});
+
+const saveBtnLoading = ref(false);
+const save = async () => {
+  saveBtnLoading.value = true;
+  const { valid } = await form.value.validate();
+  if (!valid) {
+    showMsg("请检查表单完整", "red");
+    saveBtnLoading.value = false;
+  }
+  try {
+    if (dialog.value.title === "添加") {
+      const { data }: { data: NyaResponse } = await axios.post(
+        "/v1",
+        formData.value
+      );
+      showMsg(data.msg, "green");
+    } else {
+      const { data }: { data: NyaResponse } = await axios.put(
+        `/v1`,
+        formData.value
+      );
+      showMsg(data.msg, "blue");
+    }
+  } catch (err: any) {
+    console.error(err);
+    showMsg(err.response.data.msg || err.message, "red");
+    return;
+  } finally {
+    saveBtnLoading.value = false;
+    dialog.value.cancel();
+    loadItems({
+      page: currentPage.value,
+      itemsPerPage: itemsPerPage.value,
+      sortBy: _sortBy.value,
+    });
+  }
+};
+
+const edit = (item: Blacklist) => {
+  formData.value = _.cloneDeep(item);
+  dialog.value.title = `编辑（${item.id}）`;
+  dialog.value.show = true;
+};
 </script>
