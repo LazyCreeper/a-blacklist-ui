5711ed7ef4519d8da8a67e0ecfc1f9ebc857da77
Lazy <2211717435@qq.com>
2024-01-06 23:14:57 +0800

    修复 路由类型丢失

--- components.d.ts
+++ components.d.ts
@@ -10,7 +10,6 @@ declare module 'vue' {
     DarkModeSwitcher: typeof import('./src/components/DarkModeSwitcher.vue')['default']
     Drawer: typeof import('./src/components/Drawer.vue')['default']
     Header: typeof import('./src/components/Header.vue')['default']
-    HelloWorld: typeof import("./src/components/HelloWorld.vue")["default"]
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     Snackbar: typeof import('./src/components/Snackbar.vue')['default']
